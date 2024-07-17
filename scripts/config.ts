3063c012f233ed9a12e6735c5d1245ede107a0df
Kid <hi@xuann.wang>
2024-07-18 00:19:24 +0800

    Add commit to script header

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,12 +1,18 @@
 import { readFileSync } from "node:fs";
 import { globbySync } from "globby";
 import { context } from "esbuild";
+import { execa } from "execa";
+
+const { stdout: commit } = await execa`git rev-parse HEAD`;
 
 const delimiter = "// ==/UserScript==";
-// TODO: append commit to version
 function getBanner(path: string) {
   const content = readFileSync(path, "utf8");
-  return content.slice(0, content.indexOf(delimiter) + delimiter.length);
+  return (
+    content.slice(0, content.indexOf(delimiter)) +
+    `// @commit      ${commit}\n` +
+    delimiter
+  );
 }
 
 export const config = globbySync("source/*/*.user.ts{,x}").map((script) =>
