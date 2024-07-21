5faf25efdf172b0d62702f87066ad3a0c10b656f
Kid <hi@xuann.wang>
2024-07-22 03:40:21 +0800

    Add check script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,6 @@
 import { config } from "./config";
 
+// @ts-expect-error https://github.com/microsoft/TypeScript/issues/59030
 for await (const context of config) {
   await context.rebuild();
   await context.dispose();
--- scripts/watch.ts
+++ scripts/watch.ts
@@ -1,6 +1,7 @@
 import { config } from "./config";
 
 // Note: does not detect header changes, need to manually restart
+// @ts-expect-error https://github.com/microsoft/TypeScript/issues/59030
 for await (const context of config) {
   await context.watch();
 }
