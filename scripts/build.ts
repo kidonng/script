d34a2840fa4406ab6846a97b1b17b2aa2354cbc2
Kid <hi@xuann.wang>
2025-04-18 15:50:38 +0800

    Remove `tsx`
    `--experimental-strip-types` still necessary for Vercel (Node 22)

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,4 @@
-import { config } from './config'
+import { config } from './config.ts'
 
 for await (const context of config) {
   await context.rebuild()
--- scripts/watch.ts
+++ scripts/watch.ts
@@ -1,4 +1,4 @@
-import { config } from './config'
+import { config } from './config.ts'
 
 // Note: does not detect header changes, need to manually restart
 for await (const context of config) {
